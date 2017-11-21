b50510a75491d41902f0253133e5e6db285db938
yslsaenderin <33855306+yslsaenderin@users.noreply.github.com>
2017-11-21 19:30:27 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World"); /*Hello World 출력하기 방구방구*/
+		System.out.println("Hello World"); /*Hello World 출력하기 방구방구* wㅗ예린/
 	}
 }
